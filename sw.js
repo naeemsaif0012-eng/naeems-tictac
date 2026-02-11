1e9e39771109919ee759f2994c260a461a6a2c08
naeemsaif0012-eng <naeemsaif0012@gmail.com>
2026-02-11 21:09:02 +0500

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,22 @@
+const cacheName = "tic-tac-toe-v1";
+
+const filesToCache = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./script.js",
+  "./manifest.json",
+  "./icon.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(cacheName).then(cache => cache.addAll(filesToCache))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
